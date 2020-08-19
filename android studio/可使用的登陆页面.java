9e0f94550852b2028996a9677be669247a9a6397
hblanx <60420735+hblanx@users.noreply.github.com>
2020-08-19 12:29:42 +0800

    Create 可使用的登陆页面.java

---
+++ android studio/可使用的登陆页面.java
@@ -0,0 +1,45 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void send(View view) {
+        EditText userName = (EditText)findViewById(R.id.user_name);
+        String StruserName = userName.getText().toString();
+        EditText userPasswd = (EditText)findViewById(R.id.user_passwd);
+        String StruserPasswd = userPasswd.getText().toString();
+        int canPass = 1;
+        if (StruserPasswd.length()<=3){
+            Toast.makeText(this,"密码太短",Toast.LENGTH_SHORT).show();
+            canPass=0;
+        }
+        if (StruserName.length()<3){
+            Toast.makeText(this,"用户名太短",Toast.LENGTH_SHORT).show();
+            canPass=0;
+        }
+        if (!StruserPasswd.equals("123456")){
+            Toast.makeText(this,"密码错误"+StruserPasswd,Toast.LENGTH_SHORT).show();
+            canPass=0;
+        }
+        if(canPass==1){
+            Toast.makeText(this,"登陆成功",Toast.LENGTH_SHORT).show();
+        }
+    }
+}
