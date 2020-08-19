6eff56c80aa7ccfd792bed30147ef19063db2f74
hblanx <60420735+hblanx@users.noreply.github.com>
2020-08-19 14:32:48 +0800

    Create 跳转功能函数.java

---
+++ android studio/跳转功能函数.java
@@ -0,0 +1,31 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+    public void _send(View view) {
+        Intent intent = new Intent(this,loginActivity.class);
+        startActivity(intent);
+    }
+    /*需要在app里先new一个activity，此例中loginActivity是activity的名字*/
+}
